5c4c62e2e30795c0d3fe9c32e0e412c8b06327b6
htmcharles <hatumacharles1@gmail.com>
2025-02-04 17:02:15 +0200

    hibernate

--- src/main/java/mymis/rca/models/Student.java
+++ src/main/java/mymis/rca/models/Student.java
@@ -1,10 +1,10 @@
 package mymis.rca.models;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "students")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,15 +26,16 @@ public class Student {
         this.dob = dob;
     }
 
-
     public int getId() { return id; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
     public int getAge() { return age; }
     public LocalDate getDob() { return dob; }
 
     public void setFirstName(String firstName) { this.firstName = firstName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; }
     public void setAge(int age) { this.age = age; }
     public void setDob(LocalDate dob) { this.dob = dob; }
 }
